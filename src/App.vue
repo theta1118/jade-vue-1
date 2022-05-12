635f9cb0bd4876b93984adead1d3161cff9c147d
jade <lan.wu@gientech.com>
2022-05-12 09:59:12 +0800

    vue3 造轮子

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <jade/>
+</template>
+
+<script>
+import jade from './components/jade.vue'
+
+export default {
+  name: 'App',
+  components: {
+    jade
+  }
+}
+</script>
