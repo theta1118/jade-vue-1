23e045116ba7ac7f599cbdef69e48b1361456bb6
jade <lan.wu@gientech.com>
2022-05-19 09:47:28 +0800

    初始化 aside 的html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,42 @@
 <template>
   <div>
-	  <Topnav/>
-    <div class="topnav">
-      <div class="logo">LOGO</div>
-      <ul class="menu">
-			<li>菜单1</li>
-			<li>菜单2</li>
-		</ul>
-    </div>
-
+    <Topnav />
     <div class="banner">
-      <h1>bubble UI</h1>
-      <h2>一个厉害的UI框架</h2>
+      <h1>轱辘UI</h1>
+      <h2>一个厉害的 UI 框架</h2>
       <p class="actions">
-        <a>Github</a>
-        <a>开始</a>
+        <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
 </template>
-
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
-export default{
-	components:{Topnav}
-}
+export default {
+  components: { Topnav },
+};
 </script>
-
 <style lang="scss" scoped>
-.topnav{
-	background: pink;
-	display: flex;
-	padding: 16px;
-	> .logo{
-		max-width: width 6em;
-		margin-right: auto;
-	}
-	> .menu{
-		display: flex;
-		white-space:nowrap;
-		flex-wrap:nowrap;
-		> .li{
-			margin:0 1em;
-		}
-	}
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
 }
 </style>
