f3c77b5c4059eebedb998bb547a22af4feee63de
theta1118 <1311134412@qq.com>
2022-06-09 11:18:01 +0800

    button支持level

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,53 +1,155 @@
 <template>
-    <button class="bubble-button"
-	 :class="`bubble-theme-${theme}`"
-	 >
-      <slot />
-    </button>
+  <button
+    class="bubble-button"
+    :class="classes"
+  >
+    <slot />
+  </button>
 </template>
 <script lang="ts">
+import { computed } from "vue";
 export default {
-	props:{
-		theme:{
-			type:String,
-			default:'button'
-		}
-	}
+  props: {
+    theme: {
+      type: String,
+      default: "button",
+    },
+    size: {
+      type: String,
+      default: "normal",
+    },
+	 level:{
+		 type:String,
+		 default:"normal",
+	 },
+	 disabled:{
+		 type:Boolean,
+		 default:false
+	 }
+  },
+  setup(props) {
+    const { theme, size,level } = props;
+    const classes = computed(() => {
+      return {
+        [`bubble-theme-${theme}`]: theme,
+        [`bubble-size-${size}`]: size,
+		  [`bubble-size-${size}`]: level,
+      };
+    });
+    return { classes };
+  },
 };
 </script>
 <style lang="scss">
-$h:32px;
-$border-color:#d9d9d9;
-$color:#333;
-$blue:#40a9ff;
-$radius:4px;
-.bubble-button{
-	box-sizing:border-box;
-	height:$h;
-	padding:0 12px;
-	cursor:pointer;
-	display: inline-flex;
-	justify-content: center;
-	align-items: center;
-	white-space: nowrap;
-	background: white;
-	color:$color;
-	border:1px solid $border-color;
-	border-radius: $radius;
-	box-shadow: 0 1px 0 fade-out(black,0.95);
-	& + &{
-		margin-left: 8px;
-	}
-	&:hover,
-	&:focus{
-		color:$blue;
-		border-color:$blue;
-	}
-	&:focus{
-		outline:none;
-	}
-	&::-moz-focus-inner{
-		border:0;
-	}
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+$red: red;
+.bubble-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+  &.bubble-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.bubble-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+  &.bubble-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.bubble-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.bubble-theme-button {
+    &.bubble-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.bubble-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.bubble-theme-link {
+    &.bubble-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.bubble-theme-text {
+    &.bubble-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.bubble-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
 }
 </style>
