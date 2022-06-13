5bc1e37a982a002776040c416b050436f896e148
theta1118 <1311134412@qq.com>
2022-06-13 11:18:29 +0800

    dialog的visible

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,21 +1,45 @@
 <template>
-  <div class="dialog-overlay"></div>
-  <div class="dialog-wrapper">
-    <header>标题</header>
-    <main>
-      <p>第一行字</p>
-      <p>第二行字</p>
-    </main>
-    <footer>
-      <Button>ok</Button>
-      <Button>cancel</Button>
-    </footer>
-  </div>
+  <template v-if="visible">
+    <div class="bubble-dialog-overlay"></div>
+    <div class="bubble-dialog-wrapper">
+      <div class="bubble-dialog">
+        <header>标题</header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button>ok</Button>
+          <Button>cancel</Button>
+        </footer>
+      </div>
+    </div>
+  </template>
 </template>
 
 <script lang="ts">
 import Button from "./Button.vue";
 export default {
-  components: { Button },
+  props: {
+    visible: {
+      type: Boolean,
+    },
+  },
+  components: {
+    Button,
+  },
 };
 </script>
+
+<style lang="scss">
+$radius: 4px;
+$border-color: #d9d9d9;
+
+.bubble-dialog {
+  background: white;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade-out(black, 0.5);
+  min-width: 15em;
+  max-width: 90%;
+}
+</style>
