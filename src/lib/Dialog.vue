db1e3aec4b15caf7d567b4b72a267598ddc53eac
lan <lan.wu@gientech.com>
2022-07-04 11:11:57 +0800

    dialog支持自定义title和content

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,16 +1,27 @@
 <template>
   <template v-if="visible">
-    <div class="bubble-dialog-overlay"></div>
+    <div
+      class="bubble-dialog-overlay"
+      @click="onClickOverlay"
+    ></div>
     <div class="bubble-dialog-wrapper">
       <div class="bubble-dialog">
-        <header>标题</header>
+        <header>
+         <slot name="title"/>
+          <span
+            @click="close"
+            class="bubble-dialog-close"
+          ></span>
+        </header>
         <main>
-          <p>第一行字</p>
-          <p>第二行字</p>
+         <slot name="content"/>
         </main>
         <footer>
-          <Button>ok</Button>
-          <Button>cancel</Button>
+          <Button
+            level="main"
+            @click="ok"
+          >OK</Button>
+          <Button @click="cancel">Cancel</Button>
         </footer>
       </div>
     </div>
@@ -23,10 +34,46 @@ export default {
   props: {
     visible: {
       type: Boolean,
+      default: false,
+    },
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true,
+    },
+    ok: {
+      type: Function,
+    },
+    cancel: {
+      type: Function,
     },
   },
   components: {
     Button,
+  },
+  setup(props, context) {
+    const close = () => {
+      context.emit("update:visible", false);
+    };
+    const onClickOverlay = () => {
+      if (props.closeOnClickOverlay) {
+        close();
+      }
+    };
+    const ok = () => {
+      if (props.ok?.() !== false) {
+        close();
+      }
+    };
+    const cancel = () => {
+      context.emit("cancel");
+      close();
+    };
+    return {
+      close,
+      onClickOverlay,
+      ok,
+      cancel,
+    };
   },
 };
 </script>
@@ -38,8 +85,72 @@ $border-color: #d9d9d9;
 .bubble-dialog {
   background: white;
   border-radius: $radius;
-  box-shadow: 0 0 3px fade-out(black, 0.5);
+  box-shadow: 0 0 3px fade_out(black, 0.5);
   min-width: 15em;
   max-width: 90%;
+
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade_out(black, 0.5);
+    z-index: 10;
+  }
+
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+
+  > header {
+    padding: 12px 16px;
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 20px;
+  }
+
+  > main {
+    padding: 12px 16px;
+  }
+
+  > footer {
+    border-top: 1px solid $border-color;
+    padding: 12px 16px;
+    text-align: right;
+  }
+
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+      height: 1px;
+      background: black;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+    }
+
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+  }
 }
 </style>
